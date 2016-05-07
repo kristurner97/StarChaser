b85c540943de11745d9fd9981b5de28d2e7e8ef6
KeeleUni <KeeleUni@NEWCCD24>
2016-05-07 11:51:13 +0100

    Add project files.

---
+++ StarChaser/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ASCOM StarChaser.Dome")>
+<Assembly: AssemblyDescription("ASCOM Dome driver for StarChaser")>
+<Assembly: AssemblyCompany("ASCOM Initiative")>
+<Assembly: AssemblyProduct("ASCOM Dome driver for StarChaser")>
+<Assembly: AssemblyCopyright("Copyright © 2016, The ASCOM Intiative")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("863cc170-bc15-4a9b-9af7-c75713327edc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("6.2.0.0")>
+<Assembly: AssemblyFileVersion("6.2.0.0")>
